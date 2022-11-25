1530b3f3adde35c970e549f88fbb6b2afddf6f38
sch3lp <sch3lp@gmail.com>
2022-11-25 16:16:02 +0100

    Added form to Create Emodle screen

--- src/main/kotlin/be/swsb/ui/pages/CreateEmodle.kt
+++ src/main/kotlin/be/swsb/ui/pages/CreateEmodle.kt
@@ -4,9 +4,28 @@ import kotlinx.html.*
 
 fun HTML.CreateEmodle(id: String = "CreateEmodle") = page {
     div {
-        h1 { +"Create your own Emodle!" }
+        h2 { +"Your puzzle" }
         form {
-
+            p {
+                +"Movie "
+                input {
+                    type = InputType.text
+                    name = "enteredSolution"
+                }
+            }
+            (1..5).forEach { idx ->
+                p {
+                    +"Hint $idx "
+                    input {
+                        type = InputType.text
+                        name = "hint$idx"
+                    }
+                }
+            }
+            input {
+                type = InputType.submit
+                value = "Create"
+            }
             a {
                 href = "/"
                 target = "_parent"
